88ef197c5e4ef99c878bb395946df65681a9470a
ShardulAswale <unrealdoc12@gmail.com>
2023-07-20 22:41:37 +0530

    url update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://ShardulAswale.github.io/Calcultors/"
+  base: "https://shardulaswale.github.io/calcultors/"
 })
